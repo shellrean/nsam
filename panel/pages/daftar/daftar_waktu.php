68507f02d81c573ab590a9846cdf22a7bc172950
Shellrean <reanyusufali@gmail.com>
2019-10-08 20:29:21 +0700

    Edit status ujian

--- panel/pages/daftar/daftar_waktu.php
+++ panel/pages/daftar/daftar_waktu.php
@@ -9,7 +9,7 @@
   }
 
   if(isset($_REQUEST['simpan'])) {
-    $sql = mysql_query("update cbt_ujian set XTokenUjian = '$_REQUEST[txt_token]', XKodeUjian = '$_REQUEST[txt_koduji]',XKodeKelas = '$_REQUEST[txt_kodkel]',XKodeJurusan = '$_REQUEST[txt_jur]', XSesi = '$_REQUEST[txt_sesi]', XKodeSoal = '$_REQUEST[txt_kodsoal]',XTglUjian = '$_REQUEST[txt_tuji]',XJamUjian = '$_REQUEST[txt_juji]',XLamaUjian='$_REQUEST[tt_durasi]',XBatasMasuk ='$_REQUEST[txt_bmasuk]',XTampil = '$_REQUEST[txt_hasil]',XStatusToken ='$_REQUEST[txt_statustoken]',XListening = '$_REQUEST[txt_listen]',XStatusUjian = '$_REQUEST[txt_suji]',XPdf='$_REQUEST[txt_xpdf]',XFilePdf='$_REQUEST[txt_filepdf]',XPdf='$_REQUEST[txt_xpdf]',XFilePdf='$_REQUEST[txt_filepdf]' where Urut = '$_REQUEST[id]'");
+    $sql = mysql_query("update cbt_ujian set XTokenUjian = '$_REQUEST[txt_token]', XKodeUjian = '$_REQUEST[txt_koduji]',XKodeKelas = '$_REQUEST[txt_kodkel]',XKodeJurusan = '$_REQUEST[txt_jur]', XSesi = '$_REQUEST[txt_sesi]', XKodeSoal = '$_REQUEST[txt_kodsoal]',XTglUjian = '$_REQUEST[txt_tuji]',XJamUjian = '$_REQUEST[txt_juji]',XLamaUjian='$_REQUEST[txt_durasi]',XBatasMasuk ='$_REQUEST[txt_bmasuk]',XTampil = '$_REQUEST[txt_hasil]',XStatusToken ='$_REQUEST[txt_statustoken]',XListening = '$_REQUEST[txt_listen]',XStatusUjian = '$_REQUEST[txt_suji]',XPdf='$_REQUEST[txt_xpdf]',XFilePdf='$_REQUEST[txt_filepdf]',XPdf='$_REQUEST[txt_xpdf]',XFilePdf='$_REQUEST[txt_filepdf]' where Urut = '$_REQUEST[id]'");
   }
 
   if(isset($_REQUEST['tambah'])){
@@ -57,10 +57,85 @@
             </tr>
           </thead>
           <tbody>
+          <?php
+            $no = 0;
+            $sql = mysql_query("select u.*,m.*,u.Urut as Urut, u.XKodeKelas as kokel from cbt_ujian u left join cbt_mapel m on m.XKodeMapel = u.XKodeMapel left join cbt_paketsoal p on p.XKodeSoal = u.XKodeSoal where u.XStatusUjian = '1'");
 
+            while($s = mysql_fetch_array($sql)):
+
+            $time1 = $s['XJamUjian'];
+            $time2 = $s['XLamaUjian'];
+
+            $secs = strtotime($time2)-strtotime("00:00:00");
+            $jamhabis = date("H:i:s",strtotime($time1)+$secs);
+            $sekarang = date("H:i:s");
+            $tglsekarang = date("Y-m-d");
+            $tglujian = $s['XTglUjian'];
+            $no++;
+
+            if($s['XStatusUjian'] == "0" || $s['XStatusUjian'] == "9" || $tglsekarang > $tglujian || $sekarang > $jamhabis) {
+              $status = "Selesai dikerjakan";
+            }
+            elseif($tglsekarang == $tglujian&&$sekarang<$time1) {
+              $status = "Akan dikerjakan";
+            }
+            else {
+              $status = "Sedang dikerjakan";
+            }
+          ?>
+          <tr>
+            <td>
+              <input type="hidden" value="<?= $s['Urutan']; ?>" id="txt_mapel<?= $s['Urutan']; ?>">
+              <?= $no; ?>
+              <input type="hidden" value="<?= $s['Urutan']; ?>" id="txt_soal<?= $s['Urutan']; ?>">
+            </td>
+            <td><?= $s['XKodeUjian']; ?></td>
+            <td><?= $s['kokel'].' - '.$s['XKodeJurusan']; ?></td>
+            <td><?= $s['XKodeMapel']. ' - '.$s['XNamaMapel']; ?></td>
+            <td><?= $s['XKodeSoal']; ?></td>
+            <td><?= $s['XTglUjian']; ?></td>
+            <td><?= $s['XJamUjian']; ?></td>
+            <td><?= $s['XBatasMasuk']; ?></td>
+            <td><?= $status; ?></td>
+            <td>
+              <a href="#myModal" id="custId" data-toggle="modal" data-id="<?= $s['Urut']; ?>" class="btn btn-sm btn-primary"><i class="icon-pencil"></i></a>
+            </td>
+          </tr>
+          <?php endwhile; ?>
           </tbody>
         </table>
       </div>
     </div>
   </div>
 </div>
+  <div class="modal fade" id="myModal" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Edit jadwal dan data ujian</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="fetched-data"></div>
+            </div>
+        </div>
+    </div>
+ 
+
+<script>
+  $(document).ready(function() {
+    $('#myModal').on('show.bs.modal', function(e) {
+      let rowid = $(e.relatedTarget).data('id');
+
+      $.ajax({
+        type : 'post',
+        url: 'daftar/edit_waktu.php',
+        data: 'urut='+ rowid,
+        success(data) {
+          $('.fetched-data').html(data);
+        }
+      })
+    })
+  })
+</script>
